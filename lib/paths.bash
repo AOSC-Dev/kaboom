54fe49ecec6499ab80ef82ab5c58897b95a4fba7
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-03 11:15:38 -0800

    lib/paths.bash: introduce $_CONTRIBDIR, $_SRCDIR

--- lib/paths.bash
+++ lib/paths.bash
@@ -1,8 +1,11 @@
 # Define paths.
 export _TOPDIR="$PWD"
 
+export _CONTRIBDIR="$_TOPDIR"/contrib
 export _DATADIR="$_TOPDIR"/data
 export _LIBDIR="$_TOPDIR"/lib
 export _PROCDIR="$_TOPDIR"/proc
+export _SRCDIR="$_TOPDIR"/sources
+
 export _STAGE0="$_TOPDIR"/stage0
 export _STAGE0_SRCDIR="$_STAGE0"/sources
