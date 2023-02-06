e5a2f4402f343e3d3e677627f546f4b05eb3f769
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-06 12:09:01 -0800

    data/scripts/stage0/glibc-pass1.bash: drop unneeded ppc64el flag hack

--- data/scripts/stage0/glibc-pass1.bash
+++ data/scripts/stage0/glibc-pass1.bash
@@ -7,14 +7,6 @@ mkdir -pv glibc-$GLIBC_VER/build || \
     aberr "Failed to create build directory for glibc-pass1: $?"
 cd glibc-$GLIBC_VER/build
 
-if [[ "$KABOOM_ARCH" = "ppc64el" ]]; then
-    abinfo "Storing default flags ..."
-    export _CFLAGS="${CFLAGS}"
-
-    abinfo "Dropping conflicting -mvsx flag ..."
-    export CFLAGS="${CFLAGS/-mpower8-vector/}"
-fi
-
 abinfo "glibc-pass1: Running configure ..."
 # Note: Set minimum kernel version to lowest branch used by mainline
 # architectures.
@@ -47,8 +39,3 @@ sed -e '/RTLDLIST=/s@/usr@@g' \
 abinfo "glibc-pass1: Finalising limits.h installation (gcc-pass1) ..."
 "$_STAGE0"/tools/libexec/gcc/$_TARGET/12.2.0/install-tools/mkheaders || \
     aberr "Failed to finalise limits.h installation (gcc-pass1) ..."
-
-if [[ "$KABOOM_ARCH" = "ppc64el" ]]; then
-    abinfo "Resetting default flags ..."
-    export CFLAGS="${_CFLAGS}"
-fi
