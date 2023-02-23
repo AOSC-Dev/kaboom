44d7030452032a39bb2d4534ff7f261fd96703b5
Mingcong Bai <jeffbai@aosc.xyz>
2023-02-23 00:44:13 -0800

    data/scripts/stage1/nano.bash: drop --host= specification

--- data/scripts/stage1/nano.bash
+++ data/scripts/stage1/nano.bash
@@ -7,7 +7,6 @@ cd nano-$NANO_VER
 abinfo "nano: Running configure ..."
 ./configure \
     --prefix=/usr \
-    --host=$_TARGET \
     --build=$_TARGET || \
     aberr "Failed to run configure for nano: $?"
 
