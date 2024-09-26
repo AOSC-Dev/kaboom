1e22f8b02c9629e5193519a874795df7aacc94b1
Mingcong Bai <jeffbai@aosc.io>
2024-09-26 18:07:33 +0800

    fix(cmake): append -latomic for i486

--- data/scripts/stage1/cmake.bash
+++ data/scripts/stage1/cmake.bash
@@ -1,9 +1,20 @@
+# FIXME: Whilst building on i486 ...
+#
+# /usr/lib/gcc/i486-aosc-linux-gnu/13.2.0/../../../../i486-aosc-linux-gnu/bin/ld: ../../Utilities/cmcppdap/libcmcppdap.a(typeof.cpp.o): undefined reference to symbol '__atomic_fetch_sub_8@@LIBATOMIC_1.0'
+# /usr/lib/gcc/i486-aosc-linux-gnu/13.2.0/../../../../i486-aosc-linux-gnu/bin/ld: /lib/libatomic.so.1: error adding symbols: DSO missing from command line
+if [[ "$KABOOM_ARCH" = "i486" ]]; then
+    abinfo "cmake: Appending -latomic to fix build ..."
+    export LDFLAGS="${LDFLAGS} -latomic"
+fi
+
 abinfo "cmake: Unpacking ..."
 tar xf "$_SRCDIR"/cmake-$CMAKE_VER.tar.gz || \
     aberr "Failed to unpack cmake: $?"
 
+cd cmake-$CMAKE_VER
+
 abinfo "cmake: Configuring CMake sources ..."
-"$SRCDIR"/bootstrap \
+./bootstrap \
     --prefix=/usr \
     --mandir=/share/man \
     --docdir=/share/doc/cmake \
@@ -14,3 +25,8 @@ make
 
 abinfo "cmake: Installing ..."
 make install
+
+if [[ "$KABOOM_ARCH" = "i486" ]]; then
+    abinfo "cmake: Restoring LDFLAGS ..."
+    export LDFLAGS="${LDFLAGS/-latomic/}"
+fi
