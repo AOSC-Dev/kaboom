25186efd833199797c7b0a704e4fb90257e65531
Mingcong Bai <jeffbai@aosc.io>
2024-09-26 18:06:56 +0800

    fix(stage0/sed): drop help2man from PATH
    FIXME: Older help2man does not build the man pages successfully.

--- data/scripts/stage0/sed.bash
+++ data/scripts/stage0/sed.bash
@@ -1,3 +1,11 @@
+# FIXME: Older help2man does not build the man pages successfully.
+if command -v help2man; then
+    abinfo "coreutils: Moving help2man out of PATH to workaround potential build failures ..."
+    HELP2MAN_PATH="$(dirname $(command -v help2man))"
+    mv -v "$HELP2MAN_PATH"/help2man \
+        "$_SRCDIR"/
+fi
+
 abinfo "sed: Unpacking ..."
 tar xf "$_SRCDIR"/sed-$SED_VER.tar.xz || \
     aberr "Failed to unpack sed: $?"
@@ -25,3 +33,10 @@ abinfo "sed: Installing ..."
 make install \
     DESTDIR="$_STAGE0" || \
     aberr "Failed to install sed: $?"
+
+# FIXME: Older help2man does not build the man pages successfully.
+if [ -e "$_SRCDIR"/help2man ]; then
+    abinfo "coreutils: Moving help2man out of PATH to workaround potential build failures ..."
+    mv -v "$_SRCDIR"/help2man \
+        "$HELP2MAN_PATH"/help2man
+fi
