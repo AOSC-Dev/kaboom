f60aaca0965a762137512e286f5652dbb2f87d3b
Mingcong Bai <jeffbai@aosc.io>
2024-09-26 18:07:17 +0800

    feat(stage1): add autobuild4 recipe

--- data/scripts/stage1/autobuild4.bash
+++ data/scripts/stage1/autobuild4.bash
@@ -1,6 +1,8 @@
 abinfo "autobuild4: Unpacking ..."
-tar xf "$_SRCDIR"/v$autobuild4_VER.tar.gz || \
+tar xf "$_SRCDIR"/v$AUTOBUILD4_VER.tar.gz || \
     aberr "Failed to unpack autobuild4: $?"
+
+cd autobuild4-$AUTOBUILD4_VER
 
 abinfo "autobuild4: Configuring CMake sources ..."
 cmake . \
@@ -15,11 +17,11 @@ make install
 
 abinfo "autobuild4: Setting prefix in configuration ..."
 echo '/usr/lib/autobuild4' \
-    > /usr/lib/autobuild4/etc/autobuild/prefix || \
+    > /etc/autobuild/prefix || \
     aberr "Failed to set prefix for autobuild4: $?"
 
-abinfo "autobuild4: Creating fake symlinks for apt ..."
-for fake in apt apt-get apt-cache; do
+abinfo "autobuild4: Creating fake symlinks for apt and more ..."
+for fake in apt apt-get apt-cache python2; do
     ln -sv true \
         /usr/bin/$fake || \
         aberr "Failed to create fake symlinks for $fake: $?"
