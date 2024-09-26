bf80fabfe118e78c1a1be89d662d5b628fabb1a2
Mingcong Bai <jeffbai@aosc.io>
2024-09-26 17:54:05 +0800

    fix(stage0/coreutils): make sure that help2man is not available

--- data/scripts/stage0/coreutils.bash
+++ data/scripts/stage0/coreutils.bash
@@ -1,3 +1,11 @@
+# FIXME: Older help2man does not build the man pages successfully.
+if command -v help2man; then
+    abinfo "coreutils: Moving help2man out of PATH to workaround potential build failures ..."
+    HELP2MAN_PATH="$(dirname $(command -v help2man))"
+    mv -v "$HELP2MAN_PATH"/help2man \
+        "$_SRCDIR"/
+fi
+
 abinfo "coreutils: Unpacking ..."
 tar xf "$_SRCDIR"/coreutils-$COREUTILS_VER.tar.xz || \
     aberr "Failed to unpack coreutils: $?"
@@ -7,7 +15,7 @@ cd coreutils-$COREUTILS_VER
 abinfo "Replacing config.* ..."
 for i in $(find -name config.guess -o -name config.sub); do
     cp -v "$_CONTRIBDIR"/automake/$(basename "$i") "$i" || \
-        aberr "Failed to copy replacement $i: $?."
+        aberr "Failed to copy replacement $i: $?"
 done
 
 abinfo "coreutils: Running configure ..."
@@ -16,7 +24,7 @@ abinfo "coreutils: Running configure ..."
     --host=$_TARGET \
     --build=$_TARGET \
     --enable-install-program=hostname \
-    --enable-no-install-program=kill,uptime || \
+    --enable-no-install-program=kill,uptime \
     aberr "Failed to run configure for coreutils: $?"
 
 abinfo "coreutils: Building ..."
@@ -27,3 +35,10 @@ abinfo "coreutils: Installing ..."
 make install \
     DESTDIR="$_STAGE0" || \
     aberr "Failed to install coreutils: $?"
+
+# FIXME: Older help2man does not build the man pages successfully.
+if [ -e "$_SRCDIR"/help2man ]; then
+    abinfo "coreutils: Moving help2man out of PATH to workaround potential build failures ..."
+    mv -v "$_SRCDIR"/help2man \
+        "$HELP2MAN_PATH"/help2man
+fi
