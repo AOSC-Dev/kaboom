a3482113fdafc98bf0eb5a123fba510daf6af5c1
Xinhui Yang <cyan@cyano.uk>
2025-07-18 18:18:07 +0800

    env-check: warn the user if bootstrapping armv7 on an arm64 system
    Not all AArch64 processors support 32-bit EL0, since starting from
    ARMv8, 32-bit EL0 and EL1 are optional. Apple M1 is one of them.

--- proc/00-env-check.bash
+++ proc/00-env-check.bash
@@ -51,7 +51,12 @@ if [ "$_ARCH" != "$KABOOM_ARCH" ] ; then
 	if systemd-detect-virt -qc ; then
 		abwarn "Make sure the host system has $_BINFMT registered in binfmt_misc registry."
 	elif [ "${_BINFMT_SKIP/$_ARCH/}" != "${_BINFMT_SKIP}" ] ; then
-		abinfo "Target binaries can run natively. No binfmt support is required."
+		if [ "$_ARCH" = "arm64" ] ; then
+			abwarn "Be aware that not all AArch64 processors are capable of running AArch32 binaries natively."
+			abwarn "Please check if your machine supports 32-bit EL0 and EL1 before continuing."
+		else
+			abinfo "Target binaries can run natively. No binfmt support is required."
+		fi
 	elif [ ! -e /proc/sys/fs/binfmt_misc/"$_BINFMT" ] ; then
 		aberr "Binfmt entry $_BINFMT does not exist in /proc/sys/fs/binfmt_misc."
 		abdie "Make sure the corresponding qemu-user-static package is installed."
